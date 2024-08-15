ff18990789b52ee43af5509fc34ff2130987b3d8
Hector Ector <163536279+AmrHector@users.noreply.github.com>
2024-08-15 23:15:35 +0000

    Made an IG Data Model

--- src/models.py
+++ src/models.py
@@ -7,23 +7,56 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+
+class Register(Base):
+    __tablename__ = 'register'
+    age = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+   
+
+class Login(Base):
+    __tablename__ = 'login'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
     person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(250), nullable=False)
+    content = Column(String(250), nullable=False)
+    person_id = Column(Integer, ForeignKey('person.id'))
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    content = Column(String(250), nullable=False)
+    person_id = Column(Integer, ForeignKey('person.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+class Share(Base):
+    __tablename__ = 'share'
+    messenger = Column(Integer, primary_key=True)
+    twitter = Column(String(250), nullable=False)
+    whatsapp = Column(String(250), nullable=False)
+    facebook = Column(String(250), nullable=False)
+    snapchat = Column(String(250), nullable=False)
+
+## Draw from SQLAlchemy base
+try:
+    result = render_er(Base, 'diagram.png')
+    print("Success! Check the diagram.png file")
+except Exception as e:
+    print("There was a problem genering the diagram")
+    raise e
+
+
+
+
+
 
     def to_dict(self):
         return {}
